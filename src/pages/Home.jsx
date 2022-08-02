5b32428513a48af55a095f774e36337afa5db335
emreeken183425 <emreeken1834@gmail.com>
2022-08-02 21:37:22 +0300

    new

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../components/Header';
+import { useState, useEffect } from 'react';
+// import data from '../helper/starterData';
+
+const Home = () => {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  console.log(tasks);
+  return (
+    <div>
+      <Header tasks={tasks} setTasks={setTasks} />
+      
+    </div>
+  );
+};
+
+export default Home;
